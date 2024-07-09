18115249daa89ff439947421668bbbf4e253f203
KailashRay <kailash.ray@kaliba.in>
2024-07-09 10:18:52 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from './FirebaseConfig';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="title-row">
+        <h1 className="title">Firebase Web App</h1>
+
+      </div>
     </div>
   );
 }
